dcf2bf6770272baee0b8aedc0c4e76c52f05ea10
Night Glow <44762336+NightGlowww@users.noreply.github.com>
2022-08-19 15:22:24 +0800

    创建管理页面

--- PHP/login.php
+++ PHP/login.php
@@ -1,10 +1,12 @@
 <?php
 $username = $_POST['Username'];
 $password = $_POST['Password'];
-$conn = new mysqli('127.0.0.1','root','Mengfei0908','Student_Information_System');
-$sql = $conn->query("SELECT Username FROM Users where Username='{$username}'and Password='{$password}'");
-$query =  mysqli_query($conn,$sql);
-$result = mysqli_fetch_array($query);
-echo $result?$result['Username']."Login succeful":"Login failure.";
-
+$conn = new mysqli('127.0.0.1', 'root', 'Mengfei0908', 'Student_Information_System');
+$sql = $conn->query("select Username from Users where Username='{$username}'and Password='{$password}'");
+$row = mysqli_fetch_assoc($sql); //取其中一条
+if ($row > 0) { //判断是否已存在
+    header('location:/HTML/admin.html');
+} else {
+    echo "Please Login";
+}
 $conn->close();
